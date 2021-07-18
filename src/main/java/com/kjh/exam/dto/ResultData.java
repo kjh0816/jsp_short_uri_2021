bb8c5478b190790bb939cee94c7fd7dc8f15bfff
kjh0816 <readshot2@gmail.com>
2021-07-18 17:51:38 +0900

    json 변환, ResultData에서 사용할 map, msg 기능을 담는 Ut.java 구현]

---
+++ src/main/java/com/kjh/exam/dto/ResultData.java
@@ -0,0 +1,44 @@
+package com.kjh.exam.dto;
+
+import java.util.Map;
+
+import lombok.Getter;
+import lombok.ToString;
+
+@ToString
+public class ResultData {
+	
+	@Getter
+	private String msg;
+	@Getter
+	private String resultCode;
+	@Getter
+	private Map<String, Object> body;
+	
+	private ResultData() {
+		
+	}
+	
+	public boolean isSuccess() {
+		return resultCode.startsWith("S-");
+	}
+	
+	public boolean isFail() {
+		return !isSuccess();
+	}
+	
+	public static ResultData from(String resultCode, String msg, Object...bodyArgs) {
+		ResultData rd = new ResultData();
+		
+		
+		rd.resultCode = resultCode;
+		rd.msg = msg;
+		rd.body = Ut.mapOf(bodyArgs);
+		
+		return rd;
+		
+		
+	}
+	
+
+}
